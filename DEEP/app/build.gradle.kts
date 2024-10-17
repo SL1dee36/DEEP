d2060f35dd53c178d3a5b650b8079894c06d2684
Nazaryan ART <84046495+SL1dee36@users.noreply.github.com>
2024-10-18 00:06:13 +0300

    2.9.3.x-beta -> 2.9.4-raw
    deep icon -> deep logo
    local mine db 1.0 -> local mine db 2.0 (more type)
    
    Updated .kt storage structure
    Moved all functions for divers to the folder
    
    ```
    com.example.deep/...
    diver/...
    handbook/...
    
    barometerFrameActivity.kt
    CompassFrameActivity.kt
    developer.kt
    MainActivity.kt
    ```
    
    Co-Authored-By: Ghostly-DeeF <143882286+Ghostly-DeeF@users.noreply.github.com>

--- DEEP/app/build.gradle.kts
+++ DEEP/app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 28
         targetSdk = 34
         versionCode = 1
-        versionName = "2.9.3"
+        versionName = "2.9.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
